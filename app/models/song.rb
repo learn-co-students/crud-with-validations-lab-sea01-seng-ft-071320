67a9a2161038165dc3062af125b35558d51a3fc4
afteralec <github email address>
2020-08-10 11:22:48 -0700

    Corrected error with ActiveRecord queries, was not an error with spec tests

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ApplicationRecord
   validates :title, presence: true
   validates :artist_name, presence: true
-  validates_uniqueness_of :title, { scope: [ :artist, :year ] }
+  validates_uniqueness_of :title, { scope: [ :artist_name, :release_year ] }
   validates :released, inclusion: { in: [ true, false ], message: 'Must be true or false.' }
   validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
   validates :release_year, presence: true, if: :released?
